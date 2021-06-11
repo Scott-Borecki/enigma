bfbabc76ad8536dc9d278bc7db4fed97675b8bef
Scott-Borecki <scottborecki@gmail.com>
2021-06-11 16:51:21 -0600

    Remove Alphabetable from Enigma class

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -18,13 +18,6 @@ RSpec.describe Enigma do
   end
 
   describe 'Object Methods' do
-    it 'can return the alphabet (plus space)' do
-      alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                  'y', 'z', ' ']
-      expect(@enigma.alphabet).to eq(alphabet)
-    end
-
     it "can return today's date as a DDMMYY string" do
       expect(@enigma.today).to be_a(String)
       expect(@enigma.today.length).to eq(6)
