6a1e53d0001114feffb946c511ffd501a55202ee
Scott-Borecki <scottborecki@gmail.com>
2021-06-11 08:59:05 -0600

    Attempt refactor on #keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -56,6 +56,15 @@ class Enigma
     @keys ||= Array.new(5) { rand(0...9) }
     return if @keys.length == 4
     @keys = [@keys[0] * 10 + @keys[1], @keys[1] * 10 + @keys[2], @keys[2] * 10 + @keys[3], @keys[3] * 10 + @keys[4]]
+    # key_array = @keys
+    # new_key = []
+    # @keys.each_with_index.map do |value, i|
+    #   i < 4 ? j = i + 1 : j = 0
+    #   new_key << key_array[i] * 10 + key_array[j] if i < 5
+    #   new_key
+    # end
+    # new_key.pop
+    # new_key
   end
 
   def offsets(date)
