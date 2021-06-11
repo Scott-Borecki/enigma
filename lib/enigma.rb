ffadf72852189f518467b1bce9e3599e57fcc311
Scott-Borecki <scottborecki@gmail.com>
2021-06-11 15:27:53 -0600

    Refactor #encrypt and #decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,23 +3,21 @@ class Enigma
   include Dateable
 
   def encrypt(message, key, date = today)
-    # FIXME: key needs to return string value in return hash.
-    cipher = Cipher.new(key, date)
-    encrypted_message = ''
-    message.chars.each_with_index.reduce('') do |encrypted, (letter, index)|
-      encrypted_message << cipher.shift_new_letter(letter, index)
+    cipher = Cipher.new(date, key)
+    encrypted = ''
+    message.chars.each_with_index do |letter, index|
+      encrypted << cipher.shift_new_letter(letter, index)
     end
-    { encryption: encrypted_message, key: key, date: date }
+    { encryption: encrypted, key: key, date: date }
   end
 
   def decrypt(ciphertext, key, date)
-    # FIXME: key needs to return string value in return hash.
-    cipher = Cipher.new(key, date)
-    decrypted_message = ''
+    cipher = Cipher.new(date, key)
+    decrypted = ''
     ciphertext.chars.each_with_index do |letter, index|
-      decrypted_message << cipher.unshift_new_letter(letter, index)
+      decrypted << cipher.unshift_new_letter(letter, index)
     end
-    { decryption: decrypted_message, key: key, date: date }
+    { decryption: decrypted, key: key, date: date }
   end
 
   # def crack(ciphertext, date = today)
